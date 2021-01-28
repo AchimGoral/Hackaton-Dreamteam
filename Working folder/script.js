76188ac4bda9e64f55475515a9bb8706e543e036
Achim Goral <achimgoral.il@gmail.com>
2021-01-28 17:53:44 +0200

    Html changes

--- Working folder/script.js
+++ Working folder/script.js
@@ -3,9 +3,11 @@
 let clearAllButton = document.querySelector('.btn-clear');
 clearAllButton.addEventListener('click', clear);
 
+let inputHeadline = document.querySelector(".form-control-headline");
+let inputText = document.querySelector(".form-control-text");
+
 function clear() {
-    let inputHeadline = document.querySelector(".form-control-headline");
-    let inputText = document.querySelector(".form-control-text");
+
     inputHeadline.value = "";
     inputText.value = "";
 }
@@ -36,22 +38,33 @@ function createCard () {
     divCardBody.appendChild(cardTitle);
     divCardBody.appendChild(breakLine);
     divCardBody.appendChild(cardText);
+    divCard.appendChild(divCardBody);
+    containerCard.appendChild(divCard);
 }
 
 // Submit the post
+
 let submitButton = document.querySelector(".btn-submit");
 
 submitButton.addEventListener("click", addContent);
 
 function addContent () {
-    let inputHeadline = document.querySelector(".form-control-headline").value;
-    let textField = document.querySelector(".form-control-text").value;
-    createCard();
-    let headline = document.createTextNode(inputHeadline);
-    cardTitle.appendChild(headline);
 
-    let text = document.createTextNode(textField);
-    cardText.appendChild(text);
+    let headlineField = document.querySelector(".form-control-headline").value;
+    let textField = document.querySelector(".form-control-text").value;
+
+    if (inputHeadline.value == "" || inputText.value == "") {
+        alert("Text fields can't be empty");
+        return; // checks for empty input and leaves the function if empty
+    } else {
+        createCard();
+
+        let headline = document.createTextNode(headlineField);
+        cardTitle.appendChild(headline);
     
-    containerCard.appendChild(divCardBody);
+        let text = document.createTextNode(textField);
+        cardText.appendChild(text);
+    
+        clear(); // clears the text fields after submitting
+    }
 }
